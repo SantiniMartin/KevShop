8c46811116badf12c9ff5c98a6f25758243e5dda
Martin Santini <Martin_Santini219@outlook.es>
2022-09-27 23:07:05 -0300

    kevshop

---
+++ app.js
@@ -0,0 +1,18 @@
+// let numero;
+// let suma;
+
+// for (let i = 1; i<10; i++){
+
+// }
+
+const myFunction = (a, b) => a + b
+
+alert(myFunction(5, 9))
+
+let persona = function(nombre, apellido, edad){
+    this.nombre = nombre,
+    this.apellido = apellido,
+    this.edad = edad
+}
+
+const nuevaPersonita = new persona("Anto", "Cazal", 17)
---
+++ suma.js
@@ -0,0 +1,9 @@
+let n1;
+let n2;
+let resultado;
+
+// Valores ingresados por el usuario
+n1 = parseInt(prompt("Ingrese un valor"));
+n2 = parseInt(prompt("Ingrese otro valor"));
+resultado = n1 + n2;
+alert(resultado)
